a67054cfe1e733780480fc754ce338d74c77568e
David Bui <109239230+MrM4ngo@users.noreply.github.com>
2026-05-26 14:03:32 -0400

    Add: reverse-a-linked-list - submission-5

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-5.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        int x = 0;
+        ListNode* temp = head;
+
+        while ((temp -> next) != (nullptr)){
+            temp = temp->next;
+            x++;
+        }
+
+        x = x-1;
+        ListNode* newhead = temp;
+        ListNode* rev = newhead;
+
+
+        for (int i = x; i >= 0; i--){
+            x = i;
+            temp = head;
+            while (x >= 0){
+                temp = temp->next;
+                x--;
+                
+            }
+            rev->next = temp;
+            rev = rev->next;
+  
+        }
+
+        rev->next = head;
+        rev->next->next = nullptr;
+        // rev = rev->next;
+        // rev->next = nullptr;
+
+        return newhead;
+        
+    }
+};
